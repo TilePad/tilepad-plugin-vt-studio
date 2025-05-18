ea9f036f1d524cdf2234038f1eb0e8a3723daf70
Jacobtread <jacobtread@gmail.com>
2025-05-18 20:59:43 +1200

    feat: enter runtime and local task set on init instead of using an extra async closure

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use plugin::VtPlugin;
 use state::{VtState, process_client_events};
 use std::rc::Rc;
 use tilepad_plugin_sdk::{setup_tracing, start_plugin};
-use tokio::task::{LocalSet, spawn_local};
+use tokio::task::LocalSet;
 
 mod action;
 mod messages;
@@ -10,21 +10,23 @@ mod plugin;
 mod state;
 
 fn main() {
-    let local_set = LocalSet::new();
     setup_tracing();
 
     let rt = tokio::runtime::Builder::new_current_thread()
         .enable_all()
         .build()
         .expect("Failed building the Runtime");
+    let local_set = LocalSet::new();
 
-    local_set.block_on(&rt, async move {
-        let (client, events) = vtubestudio::Client::builder()
-            .retry_on_disconnect(false)
-            .build_tungstenite();
-        let state = Rc::new(VtState::new(client));
-        let plugin = VtPlugin::new(state.clone());
-        spawn_local(process_client_events(state, events));
-        start_plugin(plugin).await;
-    });
+    let _rt_guard = rt.enter();
+    let _guard = local_set.enter();
+
+    let (client, events) = vtubestudio::Client::builder()
+        .retry_on_disconnect(false)
+        .build_tungstenite();
+    let state = Rc::new(VtState::new(client));
+    let plugin = VtPlugin::new(state.clone());
+
+    local_set.spawn_local(process_client_events(state, events));
+    local_set.block_on(&rt, start_plugin(plugin));
 }
